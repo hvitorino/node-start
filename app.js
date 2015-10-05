12c2d4a6e103b0ebfc944f0a31c1db0d7dfd73f6
Hamon Vitorino <hamon.vitorino@gmail.com>
2015-10-05 12:30:56 -0300

    New router

--- app.js
+++ app.js
@@ -24,8 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // register all routes in ./routes directory
-var appRouter = new AppRouter();
-appRouter.route(app);
+AppRouter.route(app);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- appRouter.js
+++ appRouter.js
@@ -1,13 +1,11 @@
 var requireDir = require('require-dir');
 
-module.exports = function () {
-    var self = this;
-
-    self.route = function (app) {
+module.exports = {
+    route: function (app) {
         var allRoutes = requireDir('./routes');
 
         Object.keys(allRoutes).forEach(function (key) {
             allRoutes[key].register(app);
         });
-    };
+    }
 };
